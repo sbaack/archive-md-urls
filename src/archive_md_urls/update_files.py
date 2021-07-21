4752f6f2371f2974662c4a10e61e5de99100660e
sbaack <s.baack@posteo.de>
2021-07-21 11:36:20 +0200

    Don't wrap gather_snapshots in get_snapshots

--- src/archive_md_urls/update_files.py
+++ src/archive_md_urls/update_files.py
@@ -20,29 +20,15 @@ async def update_files(files: list[Path]) -> None:
     changed_urls: int = 0
     for file in files:
         md_source: str = file.read_text(encoding="utf-8")
+        date, urls = scan_md(md_source, file)
         # Call API and collect snapshots
-        wayback_urls: dict[str, Optional[str]] = await get_snapshots(md_source, file)
+        wayback_urls: dict[str, Optional[str]] = await gather_snapshots(urls, date)
         # Update links in file source and write file
         updated_md_source: str = update_md_source(md_source, wayback_urls)
         file.write_text(updated_md_source, encoding="utf-8")
         changed_urls += len([item for item in wayback_urls.values() if item])
     print(f"Changed {changed_urls} {'URL' if changed_urls == 1 else 'URLs'} "
           f"in {len(files)} {'file' if len(files) == 1 else 'files'}.")
-
-
-async def get_snapshots(md_source: str, md_file: Path) -> dict[str, Optional[str]]:
-    """Scan Markdown file and call Wayback Machine API to create list of url-snapshot pairs.
-
-    Args:
-        md_source (str): Contents of the Markdown file
-        md_file (Path): Path of Markdown file
-
-    Returns:
-        dict[str, Optional[str]]: API call results with original URL as keys and Wayback
-                                  snapshot URLs as values
-    """
-    date, urls = scan_md(md_source, md_file)
-    return await gather_snapshots(urls, date)
 
 
 def update_md_source(md_source: str, wayback_urls: dict[str, Optional[str]]) -> str:
